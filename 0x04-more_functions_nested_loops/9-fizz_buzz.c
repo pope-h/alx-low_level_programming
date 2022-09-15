4aa7d6ef77bf109848cd904e85f1c2ae1c197aff
Ekarika Nsemeke <hamatthan@gmail.com>
2022-09-15 15:30:57 -0700

    prints the numbers from 1 to 100 with FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,12 @@ int main(void)
 		{
 			if ((i % 15) == 0)
 				printf("FizzBuzz ");
+			else if (i == 100)
+				printf("Buzz");
 			else if ((i % 5) == 0)
 				printf("Buzz ");
 			else if ((i % 3) == 0)
 				printf("Fizz ");
-			else if (i == 100)
-				printf("%d", i);
 			else
 				printf("%d ", i);
 		}
