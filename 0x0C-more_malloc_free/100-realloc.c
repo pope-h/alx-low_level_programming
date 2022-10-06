db901f0ed7f3bf5bf7b3c65c7c8938f55f81cd9b
pope-h <41937466+pope-h@users.noreply.github.com>
2022-10-06 22:01:30 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,43 +1,38 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 /**
- * _realloc - function that reallocates a memory block using malloc and free
- * @ptr: pointer to the previous memory
- * @old_size: size, in bytes, of the allocated space for ptr
- * @new_size: new size, in bytes of the new memory block
- * Return: ptr with new memory allocated, if failed return NULL
+ * _realloc - reallocates a memory block using malloc and free.
+ * @ptr: pointer to previously allocated memory
+ * @old_size: size of allocated space for ptr
+ * @new_size: size of newly allocated space
+ * Return: pointer to newly allocated memory, or NULL if failure
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p2;
-	unsigned int i = 0;
+	char *p;
+	unsigned int i, max = new_size;
+	char *oldp = ptr;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (!ptr)
-		return (malloc(new_size));
-	p2 = malloc(new_size);
+	else if (new_size == old_size)
+		return (ptr);
 
-	if (!p2)
+	p = malloc(new_size);
+	if (p == NULL)
 		return (NULL);
-	if (new_size < old_size)
-		while (i <  new_size)
-		{
-			((char *)p2)[i] = ((char *)ptr)[i];
-			i++;
-		}
 	if (new_size > old_size)
-		while (i < old_size)
-		{
-			((char *)p2)[i] = ((char *)ptr)[i];
-			i++;
-		}
+		max = old_size;
+	for (i = 0; i < max; i++)
+		p[i] = oldp[i];
 	free(ptr);
-	return (p2);
-
+	return (p);
 }
